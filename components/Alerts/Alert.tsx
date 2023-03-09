fbf4b0df46f87e4e40328abeda3e474c02877929
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2023-03-09 18:31:42 +0100

    bugfix: updated alert component

---
+++ components/Alerts/Alert.tsx
@@ -0,0 +1,30 @@
+/** @format */
+
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  CloseButton,
+} from "@chakra-ui/react";
+
+interface AlertProps {
+  type: "success" | "error" | "warning" | "info";
+  message: string;
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+}
+export default function AlertComponent(props: AlertProps) {
+  const { type, message, setState } = props;
+
+  return (
+    <Alert mb={3} status={type}>
+      <AlertIcon />
+      <AlertDescription>{message}</AlertDescription>
+      <CloseButton
+        position="absolute"
+        right="8px"
+        top="8px"
+        onClick={() => setState(false)}
+      />
+    </Alert>
+  );
+}
